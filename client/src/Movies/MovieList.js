cf2bf60ab90f7fb0bfd230bfa689837918cdfcbc
U-steam-PC\steam <EricW87@github.com>
2019-11-01 23:55:39 -0700

    finished part 2

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from 'axios';
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovies(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -17,11 +20,14 @@ const MovieList = props => {
     
     getMovies();
   }, []);
+  console.log(movies);
   
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
