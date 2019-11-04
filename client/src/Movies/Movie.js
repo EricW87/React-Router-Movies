a7470dfa93fda81c69103cf5a35eecbbfba31054
U-steam-PC\steam <EricW87@github.com>
2019-11-03 17:27:25 -0800

    finished MVP; all stretch goals

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
-  console.log(props.match.params.id);
+  //console.log(props.match.params.id);
   useEffect(() => {
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
@@ -23,36 +24,22 @@ const Movie = (props) => {
   }, [props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+     const addToSavedList = props.addToSavedList;
+     addToSavedList(movie)
+   }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
 
   const { title, director, metascore, stars } = movie;
+  movie.id = props.match.params.id;
 
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
+      <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
+      <button className="save-button" onClick={() => saveMovie()}>Save</button>
     </div>
   );
 }
